d3976ceded3034d54b315620d246848990126a3b
Franco Celedonio <igol@192.168.254.101>
2024-06-06 19:06:36 +0800

    Adds telescope and treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript"},
+        ensure_installed = {"lua", "javascript", "elixir", "eex"},
         highlight = { enable = true },
         indent = { enable = true }
       })
